1f8281f4fc911591b4ed9c2e9e23129161af5f35
senaygui <senaygeletaw@gmail.com>
2025-02-01 10:20:06 +0300

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,10 @@ AdminUser.create!(first_name: "admin",   last_name: "user",
 									email: "admin4@gmail.com", 
 									password: "1234567",  
 									role: "admin") 
-AdminUser.create!(first_name: "#{Rails.application.credentials.production[:first_name]}",   
-	               last_name: "#{Rails.application.credentials.production[:last_name]}",
- 				   email: "#{Rails.application.credentials.production[:admin_email]}", 
- 				   password: "#{Rails.application.credentials.production[:seed_password]}",
-				   password_confirmation: "#{Rails.application.credentials.production[:seed_password]}",  
- 				   role: "#{Rails.application.credentials.production[:role]}") if Rails.env.production?
+AdminUser.create!(first_name: Rails.application.credentials.production[:first_name],   
+                 last_name: Rails.application.credentials.production[:last_name],
+                 email: Rails.application.credentials.production[:admin_email], 
+                 password: Rails.application.credentials.production[:seed_password],
+                 password_confirmation: Rails.application.credentials.production[:seed_password],  
+                 role: Rails.application.credentials.production[:role]) if Rails.env.production?
+
